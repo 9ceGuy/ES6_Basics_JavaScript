5e84867c696aaa891b94dedd255aade9c26a7b2c
JEBREED <info@jetbreed.com>
2023-07-05 09:52:38 +0100

    Implementing full begin!

--- import_named.js
+++ import_named.js
@@ -1,5 +1,5 @@
-c
-import { name1, name2 } from "./person.js";
+import { name1, name2, bright } from "./import_named_person";
 
 document.getElementById("display1").innerHTML = "Our first name is: " + name1;
 document.getElementById("display2").innerHTML = "Our first name is: " + name2;
+document.getElementById("display3").innerHTML = "Our first name is: " + bright;
