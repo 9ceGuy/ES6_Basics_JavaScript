f3048fd7191a52fe8e506eeaa5ca244fd1e76152
JEBREED <info@jetbreed.com>
2023-07-06 09:00:40 +0100

    Implementing full begin!

--- import_default.js
+++ import_default.js
@@ -1,6 +1,6 @@
 
 
-const groupOfPeople = () =>{
+ let groupOfPeople1 = () =>{
     const name1 = "Chris";
     const name2 = "Elvis";
     const name3 = "Stephen";
@@ -10,5 +10,18 @@ const groupOfPeople = () =>{
     return [name1, name2, name3, name4, name5];
 }
 
+let groupOfPeople2 = () =>{
+    const name6 = "Chris1";
+    const name7 = "Elvis1";
+    const name8 = "Stephen1";
+    const name9 = "Anthony1";
+    const name10 = "Ibiye1";
+    
+    return [name6, name7, name8, name9, name10];
+}
+
+const groupOfPeople = [...groupOfPeople1(), ...groupOfPeople2()];
+
+// console.log(groupOfPeople);
 
 export default groupOfPeople;
